c90f36564bdadc503f79bb88e11c91138735e0e6
Romain Manchado <romain.manchado@bell.ca>
2023-01-02 11:11:18 -0500

    change nodejs server port

--- src/back-end/server.mjs
+++ src/back-end/server.mjs
@@ -22,7 +22,7 @@ fastify.get('/api/player', async (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 8080 })
+    await fastify.listen({ port: 3000 })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
