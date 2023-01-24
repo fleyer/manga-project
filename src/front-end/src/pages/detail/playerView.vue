e84fd6ec12b3656aeb2ac90fa5c8b95a1487662a
Romain Manchado <romain.manchado@bell.ca>
2023-01-23 23:20:33 -0500

    finish player

--- src/front-end/src/pages/detail/playerView.vue
+++ src/front-end/src/pages/detail/playerView.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 import type { Ref } from 'vue'
-import { defineProps } from 'vue'
+import { defineProps, withDefaults } from 'vue'
 import nProgress from 'nprogress'
 import type { MangaDetail, MangaHistory } from '~/types'
 
-const props = defineProps<{ autoPlay?: { type: boolean, default: false} }>()
+const props = withDefaults(defineProps<{ autoPlay?: boolean }>(),{autoPlay: false})
 
 const mangaStore = useMangaStore()
 const historyStore = useHistoryStore()
@@ -19,10 +19,13 @@ const { pushHistory } = historyStore
 const videoPlayer = ref<HTMLVideoElement>()
 const visible = ref<boolean>(false)
 const isPlaying = ref<boolean>(!!props.autoPlay)
+const historicalProgress = computed(() => (detail.value?.manga_id ? history.value[detail.value.manga_id]?.progress : 0))
+const currentTime = ref<number>(0)
+const displayedTime = computed(() => `${String(Math.round(currentTime.value / 60)).padStart(2,"0")}:${String(Math.round(currentTime.value % 60)).padStart(2,'0')}`)
 let tryNumber = 0
 
 watchEffect((onInvalidate) => {
-  const interval = setInterval(() => updateProgress(detail.value!, videoPlayer.value!), 1000)
+  const interval = setInterval(() => detail.value && updateProgress(detail.value!, videoPlayer.value!), 1000)
   props.autoPlay && videoPlayer.value?.setAttribute('autoplay', '')
 
   onInvalidate(() => {
@@ -31,13 +34,18 @@ watchEffect((onInvalidate) => {
 })
 
 function updateProgress(detail: MangaDetail, videoPlayer: HTMLVideoElement) {
-  isPlaying.value && pushHistory({
+  const savedProgress = history.value[detail!.manga_id]?.progress || -1
+  const progress = calculateProgress(videoPlayer)
+
+  currentTime.value = videoPlayer.currentTime
+
+  isPlaying.value && progress > savedProgress && pushHistory({
     id: detail.id,
     title: detail.title,
     manga_title: detail.manga_title,
     manga_id: detail.manga_id,
     image_link: detail.image_link,
-    progress: calculateProgress(videoPlayer),
+    progress,
     episode: detail!.current_episode.number,
     source: detail.source,
     subtitle: detail.subtitle,
@@ -83,7 +91,7 @@ function continueWatching(
   history: Record<string, MangaHistory>,
   videoPlayer?: HTMLVideoElement,
 ) {
-  const mangaHistory = detail!.value!.manga_title ? history[detail!.value!.manga_title] : null
+  const mangaHistory = detail?.value?.id ? history[detail!.value!.manga_id] : null
 
   if (
     mangaHistory
@@ -99,6 +107,13 @@ function calculateProgress(videoPlayer: HTMLVideoElement): number {
 
 function play(player : HTMLVideoElement){
   (player.paused ? player.play : player.pause).bind(player)()
+}
+
+function updatePlayback(event : Event) {
+  const mangaHistory = history.value[detail!.value!.manga_id]
+  mangaHistory.progress = parseInt((event.target as HTMLInputElement).value)
+
+  videoPlayer!.value!.currentTime = videoPlayer!.value!.duration * mangaHistory.progress / 100
 }
 </script>
 
@@ -123,12 +138,30 @@ function play(player : HTMLVideoElement){
     @loadedmetadata="onLoadMetaData"
     @error="onError"
   />
-  <div class="controls absolute bottom-0 py-4 px-6 text-lg w-full flex items-end">
-    <button class="btn bg-transparent hover:bg-transparent" ><div class="i-carbon:skip-back-filled"></div></button>
-    <button class="btn bg-transparent hover:bg-transparent" @click="play(videoPlayer!)">
-      <div :class="isPlaying ? 'i-carbon:pause-filled' : 'i-carbon:play-filled-alt'"></div>
-    </button>
-    <button class="btn bg-transparent hover:bg-transparent" ><div class="i-carbon:skip-forward-filled"></div></button>
+  <div class="controls absolute bottom-0 py-4 px-6 text-lg w-full flex flex-col justify-end items-center">
+    <div class="slider w-full flex">
+      <input
+        type="range"
+        min="0"
+        max="100"
+        class="flex-1"
+        :value="historicalProgress"
+        @input="updatePlayback"
+      />
+    </div>
+    <div class="toolbar flex relative w-full justify-center">
+      <button class="btn bg-transparent hover:bg-transparent" ><div class="i-carbon:skip-back-filled"></div></button>
+        <button class="btn bg-transparent hover:bg-transparent" @click="play(videoPlayer!)">
+          <div :class="isPlaying ? 'i-carbon:pause-filled' : 'i-carbon:play-filled-alt'"></div>
+        </button>
+      <button class="btn bg-transparent hover:bg-transparent" ><div class="i-carbon:skip-forward-filled"></div></button>
+
+      <div class="absolute top-0 right-0">
+        <div class="w-20 flex justify-center">{{ displayedTime }}</div>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
